38c1789259d5b1c00466ea7162bc7cfe1c519a61
caroljlsun <61421828+caroljlsun@users.noreply.github.com>
2020-03-07 18:11:48 +0000

    plotting lineage marker index with nmp index

--- indeterminacy.R
+++ indeterminacy.R
@@ -10,8 +10,8 @@ library(ggpubr)
 library(extrafont)
 
 #setting up processing functions, ripped from Kane's previous code
-processcsv_hes_cdh_1 <- function(my_df, gene_names, my_sample_hes_cdh_1){
-  my_df$sample_ID = my_sample_hes_cdh_1
+processcsv_hes_cdh <- function(my_df, gene_names, my_sample_hes_cdh){
+  my_df$sample_ID = my_sample_hes_cdh
   csv <- select(my_df, starts_with("Intensity"), "ID", "sample_ID")
   colnames(csv)[1] <- gene_names
   colnames(csv)[2] <- "cell_ID"
@@ -22,41 +22,58 @@ processcsv_hes_cdh_1 <- function(my_df, gene_names, my_sample_hes_cdh_1){
 #navigating to the right cdh6 hes6 folder
 
 
-dir_hes_cdh <- list.dirs(path = "./dataCsv/sox2_tbxta_hes6_cdh6")[-1]
+dir_hes_cdh <- list.dirs(path = "./dataCsv/hes6_cdh6")[-1]
 
 my_hes_cdh_csv<- dir_hes_cdh %>% 
   map(~list.files(path = ., pattern="\\.csv$", full.names = TRUE)) 
 
-my_sample_no_18 <- c()
-for (dir18ss in my_dir18ss_csv){
-  my_ID_18 <- rm_between(dir18ss, "sample_", "/", extract=TRUE) %>%
+my_sample_hes_cdh <- c()
+for (dir_hes_cdh in my_hes_cdh_csv){
+  my_ID <- rm_between(dir_hes_cdh, "sample_", "/", extract=TRUE) %>%
     unlist() %>%
     unique()
-  my_sample_no_18 <- c(my_sample_no_18, my_ID_18)
+  my_sample_hes_cdh<- c(my_sample_hes_cdh, my_ID)
 }
 
-my_sample_no_18 <- as.integer(my_sample_no_18)
+my_sample_hes_cdh <- as.integer(my_sample_hes_cdh)
 
-gene_names = c("sox2", "tbxta")
+gene_names = c("sox2", "tbxta","cdh6","hes6")
 
-my_csv_files_18 <- lapply(my_dir18ss_csv, function(x){ lapply(x, FUN = read.csv, header = TRUE, skip = 3)}) 
+my_csv_files_cdh_hes <- lapply(my_hes_cdh_csv, function(x){ lapply(x, FUN = read.csv, header = TRUE, skip = 3)}) 
 
-my_csv_files_proc_18 <- vector("list", length = length(my_csv_files_18))
-my_csv_files_proc_merged_18 <- vector("list", length = length(my_csv_files_18))
+#merge sox2 and tbxta
+my_csv_files_proc_hes_cdh_a <- vector("list", length = length(my_csv_files_cdh_hes))
+my_csv_files_proc_merged_hes_cdh_a <- vector("list", length = length(my_csv_files_cdh_hes))
 
-for (i in 1:length(my_csv_files_18)){
+for (i in 1:length(my_csv_files_cdh_hes)){
   for (j in 1:length(gene_names)){
-    my_csv_processed_18 <- processcsv18(my_df = my_csv_files_18[[i]][[j]], 
+    my_csv_processed_cdh_hes_a <- processcsv_hes_cdh(my_df = my_csv_files_cdh_hes[[i]][[j]], 
                                         gene_names = gene_names[j],
-                                        my_sample_no_18 = my_sample_no_18[i])
-    my_csv_files_proc_18[[i]][[j]] <- my_csv_processed_18
+                                        my_sample_hes_cdh = my_sample_hes_cdh[i])
+    my_csv_files_proc_hes_cdh_a[[i]][[j]] <- my_csv_processed_cdh_hes_a
   }
-  my_csv_files_proc_merged_18[[i]] <- merge(my_csv_files_proc_18[[i]][[1]], my_csv_files_proc_18[[i]][[2]], by = c("cell_ID", "sample_ID"))
-  #my_csv_files_proc_merged_18[[i]] <- bind_rows(list(my_csv_files_proc_18[i][1],my_csv_files_proc_18[i][2],my_csv_files_proc_18[i][3]))
+  my_csv_files_proc_merged_hes_cdh_a[[i]] <- merge(my_csv_files_proc_hes_cdh_a[[i]][[1]], my_csv_files_proc_hes_cdh_a[[i]][[2]], by = c("cell_ID", "sample_ID"))
+  
+}
+# merge cdh5 and hes6
+my_csv_files_proc_hes_cdh_b <- vector("list", length = length(my_csv_files_cdh_hes))
+my_csv_files_proc_merged_hes_cdh_b <- vector("list", length = length(my_csv_files_cdh_hes))
+
+for (i in 1:length(my_csv_files_cdh_hes)){
+  for (j in 1:length(gene_names)){
+    my_csv_processed_cdh_hes_b <- processcsv_hes_cdh(my_df = my_csv_files_cdh_hes[[i]][[j]], 
+                                                     gene_names = gene_names[j+2],
+                                                     my_sample_hes_cdh = my_sample_hes_cdh[i])
+    my_csv_files_proc_hes_cdh_b[[i]][[j]] <- my_csv_processed_cdh_hes_b
+  }
+  my_csv_files_proc_merged_hes_cdh_b[[i]] <- merge(my_csv_files_proc_hes_cdh_b[[i]][[1]], my_csv_files_proc_hes_cdh_b[[i]][[2]], by = c("cell_ID", "sample_ID"))
 }
 
 
-df_combined_18 <- my_csv_files_proc_merged_18 %>%
+#my_csv_files_proc_merged_hes_cdh <- vector("list", length = length(my_csv_files_cdh_hes))
+
+
+df_combined_a <- my_csv_files_proc_merged_hes_cdh_a %>%
   reduce(rbind) %>%
   group_by(sample_ID) %>%
   arrange(sample_ID, cell_ID) %>%
@@ -64,5 +81,62 @@ df_combined_18 <- my_csv_files_proc_merged_18 %>%
   ungroup()%>%
   mutate(nm_index = .[[5]]-.[[6]])
 
+
+df_combined_b <- my_csv_files_proc_merged_hes_cdh_b %>%
+  reduce(rbind) %>%
+  group_by(sample_ID) %>%
+  arrange(sample_ID, cell_ID) 
+  
+
+df_combined <- merge(df_combined_a, df_combined_b, by = c("cell_ID", "sample_ID")) %>% 
+  mutate_at(.funs = list(normalise = ~((.-min(.))/max(.-min(.)))), .vars = 8:9) %>%
+  ungroup()%>%
+  mutate(lineage_index = .[[10]]-.[[11]])
+
 #plotting
 
+p_nmps_only <- ggplot(df_combined, aes(x=nm_index, fill = factor(sample_ID)))+
+  geom_histogram(binwidth = 0.2, color="black")+
+  scale_x_continuous(name = "NMindex",limits=c(-1, 1))+
+  theme_minimal()+
+  scale_fill_manual(values=plasma(n=length(my_csv_files_cdh_hes)))+
+  theme(text = element_text(size = 15, family = "sans"))+
+  ggtitle("NMP distribution")+
+  #theme(legend.position = "none")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+line_nmps_only <- ggplot(df_combined, aes(x=nm_index, y=))+
+  geom_density(colour = "red")+
+  theme_minimal()+
+  ggtitle("NMP distribution")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(text = element_text(size = 15, family = "sans"))
+
+# plotting the lineage marker index
+p_lineage_only <- ggplot(df_combined, aes(x=lineage_index, fill = factor(sample_ID)))+
+  geom_histogram(binwidth = 0.2, color="black")+
+  scale_x_continuous(name = "Lineage index",limits=c(-1, 1))+
+  theme_minimal()+
+  scale_fill_manual(values=plasma(n=length(my_csv_files_cdh_hes)))+
+  theme(text = element_text(size = 15, family = "sans"))+
+  ggtitle("Lineage marker distribution")+
+  #theme(legend.position = "none")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+p_lineage_only
+
+line_lineage <- ggplot(df_combined, aes(x=lineage_index, y=))+
+  geom_density(colour = "blue")+
+  theme_minimal()+
+  ggtitle("Lineage marker distribution")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(text = element_text(size = 15, family = "sans"))
+
+line_lineage
+
+#plotting both nm index and lineage
+
+p_nmp_lineage <- ggplot(df_combined, aes(x=nm_index, y=lineage_index))+
+  geom_point(color = factor(df_combined$sample_ID))
+p_nmp_lineage
